c6dfe0119acfe7afa5384e343584b37ed86646f6
MegY <meghdadyazdi@yahoo.com>
2019-11-22 10:21:19 +0000

    Adding more chaining method

--- script_Method-Chaining.js
+++ script_Method-Chaining.js
@@ -1,5 +1,18 @@
-$(document).ready(function() {
- $("#button1").mouseenter(function() {
-        $("#button1").removeClass('makeRed').addClass('makeBorder');
-});
+$(document).ready(function () {
+    // $("#button1").mouseenter(function () {
+    //     $("#button1").removeClass('makeRed').addClass('makeBorder');
+    // });
+    $("#button1").click(function () {
+        $("#para1").slideToggle(2000);
+    });
+
+        // hides/shows paragraphs when either button is clicked
+    // $("#button2").click(function () {
+    //         $("#para2").hide(2000).show(2000);
+    // });
+
+    $("#button2").click(function () {
+            $("#para2").fadeIn().fadeOut();
+    });
+    
 }); 
